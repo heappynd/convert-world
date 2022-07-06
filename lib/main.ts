d448c4f126c2972b9d21c437472b07d7048987c5
heappynd <heappynd@163.com>
2022-07-07 01:12:34 +0800

    feat: data-storage

---
+++ lib/main.ts
@@ -0,0 +1,25 @@
+const unit = ['B', 'kB', 'MB', 'GB', 'TB', 'PB', 'EB'] as const
+
+type Unit = typeof unit[number]
+
+export function c_data_storge(
+  amount: number,
+  from: Unit,
+  to: Unit,
+  decimals: number = 2
+) {
+  if (amount === 0) return 0
+  if (from === to) return from
+  let fromIdx = unit.indexOf(from)
+  let toIdx = unit.indexOf(to)
+  let flag = fromIdx < toIdx
+  if (!flag) {
+    ;[fromIdx, toIdx] = [toIdx, fromIdx]
+  }
+  for (let i = fromIdx; i < toIdx; i++) {
+    amount = flag ? amount / 1024 : amount * 1024
+  }
+  return amount.toFixed(decimals)
+}
+
+console.log(c_data_storge(1, 'GB', 'B'))
---
+++ test.js
@@ -0,0 +1,3 @@
+import { c_data_storge } from './dist/covert-world.es'
+
+c_data_storge(1025,'MB','GB')
